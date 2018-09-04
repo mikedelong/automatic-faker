b0968822524246eff15aca2157536fd1e2a5ef4c
mikedelong <mikedelong@outlook.com>
2018-09-04 13:08:17 -0400

    feature: first working version with a FileHandler

--- code/console_file_logger.py
+++ code/console_file_logger.py
@@ -4,16 +4,25 @@ from time import time
 if __name__ == '__main__':
     start_time = time()
 
-    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    console_formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s : %(message)s')
     logger = logging.getLogger('main')
     logger.setLevel(logging.DEBUG)
+
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    console_handler.setFormatter(console_formatter)
     console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler('../output/automatic-faker.log', mode='a', encoding='utf-8', delay=False)
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
     logger.info('started')
 
     logger.info('done')
+
     finish_time = time()
     elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
     elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
